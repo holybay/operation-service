c9ce2d384f9741e799be40a76bcd575c8a2c4f74
Vlad Golubev <holybay1886@gmail.com>
2025-04-15 01:22:28 +0300

    Added GET /account/{accountId}/limits end-point.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/api/controller/LimitController.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/api/controller/LimitController.java
@@ -2,16 +2,23 @@ package com.idftechnology.transactionlimitsservice.api.controller;
 
 import com.idftechnology.transactionlimitsservice.api.dto.LimitCreateDto;
 import com.idftechnology.transactionlimitsservice.api.dto.LimitOutDto;
+import com.idftechnology.transactionlimitsservice.core.platform.util.ApplicationConstant;
 import com.idftechnology.transactionlimitsservice.core.service.api.LimitFacade;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZoneOffset;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,5 +32,14 @@ public class LimitController {
                                            @RequestBody @Valid LimitCreateDto createDto) {
         LimitOutDto created = limitFacade.add(accountId, createDto);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LimitOutDto>> getAll(@PathVariable Long accountId,
+                                                    Pageable pageable,
+                                                    @RequestHeader(ApplicationConstant.ZONE_OFFSET)
+                                                    ZoneOffset zoneOffset) {
+        List<LimitOutDto> limits = limitFacade.getAll(accountId, pageable, zoneOffset);
+        return new ResponseEntity<>(limits, HttpStatus.OK);
     }
 }
