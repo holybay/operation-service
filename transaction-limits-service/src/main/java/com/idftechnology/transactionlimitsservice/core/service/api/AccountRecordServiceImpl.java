f331c59a1e2ada9ea493c108a1ab2f22b7395e27
Vlad Golubev <holybay1886@gmail.com>
2025-04-15 00:56:45 +0300

    Added AccountRecord service and impl.

---
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/service/api/AccountRecordServiceImpl.java
@@ -0,0 +1,29 @@
+package com.idftechnology.transactionlimitsservice.core.service.api;
+
+import com.idftechnology.transactionlimitsservice.core.repository.api.AccountRecordRepository;
+import com.idftechnology.transactionlimitsservice.core.repository.entity.AccountRecord;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountRecordServiceImpl implements AccountRecordService {
+
+    private final AccountRecordRepository repository;
+
+    @Transactional
+    @Override
+    public void activate(Long accountId) {
+        repository.saveAndFlush(AccountRecord.builder()
+                                             .accountId(accountId)
+                                             .isActivated(true)
+                                             .build());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public boolean isActive(Long accountId) {
+        return repository.existsById(accountId);
+    }
+}
