6aa04d87ae8f1c8951ccd4756de2759ff399b9b6
Vlad Golubev <holybay1886@gmail.com>
2025-04-15 01:06:27 +0300

    Updated add method to check and init limits first.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/service/impl/LimitFacadeImpl.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/service/impl/LimitFacadeImpl.java
@@ -3,6 +3,7 @@ package com.idftechnology.transactionlimitsservice.core.service.impl;
 import com.idftechnology.transactionlimitsservice.api.dto.LimitCreateDto;
 import com.idftechnology.transactionlimitsservice.api.dto.LimitOutDto;
 import com.idftechnology.transactionlimitsservice.core.service.api.LimitFacade;
+import com.idftechnology.transactionlimitsservice.core.service.api.LimitInitializeService;
 import com.idftechnology.transactionlimitsservice.core.service.api.LimitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Service;
 public class LimitFacadeImpl implements LimitFacade {
 
     private final LimitService limitService;
+    private final LimitInitializeService limitInitService;
 
     @Override
     public LimitOutDto add(Long accountId, LimitCreateDto dto) {
+        limitInitService.checkAndInit(accountId, dto.getZone());
         return limitService.add(accountId, dto);
     }
 
