1aaf04dc07546bd5055030438b9ff3545a7391ce
Vlad Golubev <holybay1886@gmail.com>
2025-04-15 01:19:39 +0300

    Added a method to get active limits.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/repository/api/LimitRepository.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/repository/api/LimitRepository.java
@@ -1,6 +1,8 @@
 package com.idftechnology.transactionlimitsservice.core.repository.api;
 
 import com.idftechnology.transactionlimitsservice.core.repository.entity.Limit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,8 @@ public interface LimitRepository extends JpaRepository<Limit, Long> {
     @Query("SELECT l FROM Limit l WHERE l.accountFrom = :account AND l.expenseCategory.name = :category")
     Optional<Limit> findLimitByCategory(@Param("account") Long accountFrom,
                                         @Param("category") String expenseCategory);
+
+    @Query("SELECT l FROM Limit l WHERE l.accountFrom = :account AND l.dateTo IS NULL")
+    Page<Limit> getActiveLimits(@Param("account") Long accountFrom, Pageable pageable);
+
 }
