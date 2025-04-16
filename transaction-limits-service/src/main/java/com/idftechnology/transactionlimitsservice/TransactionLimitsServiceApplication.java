ca21acbb33de057ebadcb72fe6315d7390728ce7
Vlad Golubev <holybay1886@gmail.com>
2025-04-16 21:53:53 +0300

    Fix: added @EnableFeignClients annotation.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/TransactionLimitsServiceApplication.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/TransactionLimitsServiceApplication.java
@@ -3,9 +3,11 @@ package com.idftechnology.transactionlimitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.idftechnology.transactionlimitsservice.core.config.property")
+@EnableFeignClients
 public class TransactionLimitsServiceApplication {
 
     public static void main(String[] args) {
