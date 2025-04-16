63747533a12fda43e15625a222a3dcbed82c4b00
Vlad Golubev <holybay1886@gmail.com>
2025-04-17 01:57:00 +0300

    Added a method to find all rates by list of currency pair and list of dates.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/repository/api/ExchangeRateRepository.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/repository/api/ExchangeRateRepository.java
@@ -2,6 +2,20 @@ package com.idftechnology.transactionlimitsservice.core.repository.api;
 
 import com.idftechnology.transactionlimitsservice.core.repository.entity.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
+
+    @Query("""
+            SELECT er FROM ExchangeRate er
+            "WHERE er.from.currencyCode IN :fromList
+            "AND er.to.currencyCode IN :toList
+            "AND er.date IN :dates""")
+    List<ExchangeRate> findRatesByPairsAndDates(@Param("fromList") Iterable<String> fromList,
+                                                @Param("toList") Iterable<String> toList,
+                                                @Param("dates") Iterable<LocalDate> dates);
 }
