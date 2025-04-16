b51fa931c2d386f521c70c19cb577663acb8d27b
Vlad Golubev <holybay1886@gmail.com>
2025-04-17 00:58:20 +0300

    Added constructors.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/service/dto/CurrencyPair.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/service/dto/CurrencyPair.java
@@ -1,10 +1,14 @@
 package com.idftechnology.transactionlimitsservice.core.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CurrencyPair {
 
     private String from;
