2fe31ac20f5eec5a449085358f92750287007c5a
Vlad Golubev <holybay1886@gmail.com>
2025-04-16 21:54:34 +0300

    Added TwelveData property class and updated application.yml file.

---
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/config/property/TwelveDataProperty.java
@@ -0,0 +1,25 @@
+package com.idftechnology.transactionlimitsservice.core.config.property;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Data
+@ConfigurationProperties(prefix = "clients.currency.")
+@Validated
+public class TwelveDataProperty {
+
+    @NotBlank
+    private String apiKey;
+
+    @NotBlank
+    private String interval;
+
+    @Positive
+    private int outputSize;
+
+    private boolean previousClose;
+
+}
