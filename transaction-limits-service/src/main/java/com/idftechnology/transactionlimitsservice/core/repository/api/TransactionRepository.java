cb3b3143bde53a643aed40abb0daf8bb56a2735b
Vlad Golubev <holybay1886@gmail.com>
2025-04-17 02:02:26 +0300

    Added find all transactions for the current month for specified expense category.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/repository/api/TransactionRepository.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/repository/api/TransactionRepository.java
@@ -2,6 +2,25 @@ package com.idftechnology.transactionlimitsservice.core.repository.api;
 
 import com.idftechnology.transactionlimitsservice.core.repository.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.OffsetDateTime;
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query(value = """
+            SELECT t
+            FROM Transaction t
+            WHERE t.accountFrom = :account
+            AND t.expenseCategory.name = :category
+            AND t.dateTime BETWEEN :start AND :end
+            ORDER BY t.dateTime DESC
+            """)
+    List<Transaction> findAllByLimitBetween(@Param("account") Long accountId,
+                                            @Param("category") String expenseCategory,
+                                            @Param("start") OffsetDateTime start,
+                                            @Param("end") OffsetDateTime end);
+
 }
