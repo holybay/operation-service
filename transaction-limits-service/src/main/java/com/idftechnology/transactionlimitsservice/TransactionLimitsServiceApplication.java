205ea169fcd99a03566eb05c20fdf773433653c2
Vlad Golubev <holybay1886@gmail.com>
2025-04-17 04:59:13 +0300

    Added Scheduler to retrieve rates daily.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/TransactionLimitsServiceApplication.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/TransactionLimitsServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.idftechnology.transactionlimitsservice.core.config.property")
 @EnableFeignClients
+@EnableScheduling
 public class TransactionLimitsServiceApplication {
 
     public static void main(String[] args) {
