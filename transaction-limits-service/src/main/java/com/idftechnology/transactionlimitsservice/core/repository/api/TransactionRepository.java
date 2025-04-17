6786a8f10f99891bd35f4032529900aa26611052
Vlad Golubev <holybay1886@gmail.com>
2025-04-17 04:13:39 +0300

    Added find all transaction with exceeded flag for by account limit.

--- transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/repository/api/TransactionRepository.java
+++ transaction-limits-service/src/main/java/com/idftechnology/transactionlimitsservice/core/repository/api/TransactionRepository.java
@@ -1,6 +1,8 @@
 package com.idftechnology.transactionlimitsservice.core.repository.api;
 
 import com.idftechnology.transactionlimitsservice.core.repository.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,5 +24,20 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
                                             @Param("category") String expenseCategory,
                                             @Param("start") OffsetDateTime start,
                                             @Param("end") OffsetDateTime end);
+
+    @Query(value = """
+            SELECT t
+            FROM Transaction t
+            WHERE t.accountFrom = :account
+            AND t.expenseCategory.name = :category
+            AND t.dateTime BETWEEN :start AND :end
+            AND t.limitExceeded = TRUE
+            ORDER BY t.dateTime DESC
+            """)
+    Page<Transaction> findAllByAccountFrom(@Param("account") Long accountFrom,
+                                           @Param("category") String expenseCategory,
+                                           @Param("start") OffsetDateTime start,
+                                           @Param("end") OffsetDateTime end,
+                                           Pageable pageable);
 
 }
